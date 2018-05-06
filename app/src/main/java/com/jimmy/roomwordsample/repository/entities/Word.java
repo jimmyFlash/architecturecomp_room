cd236bedbbd0f5533f2b817ca62c10b6f50e29cd
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2018-05-06 20:26:02 +0200

    - create project and add room and LD dependencies - create entity for word - create doa interface

---
+++ app/src/main/java/com/jimmy/roomwordsample/repository/entities/Word.java
@@ -0,0 +1,40 @@
+package com.jimmy.roomwordsample.repository.entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Word that describes a word Entity. You need a constructor and
+ * a "getter" method for the data model class, because that's how
+ * Room knows to instantiate your objects.
+ *
+ * To make the Word class meaningful to a Room database, you need to annotate it.
+ * Annotations identify how each part of this class relates to an entry in the database.
+ * Room uses this information to generate code.
+ *
+ * @Entity(tableName = "word_table")
+ * Each @Entity class represents an entity in a table. Annotate your class declaration to indicate that it's an entity.
+ *    Specify the name of the table if you want it to be different from the name of the class.
+ *    @PrimaryKey
+ *    Every entity needs a primary key. To keep things simple, each word acts as its own primary key.
+ *    @NonNull
+ *    Denotes that a parameter, field, or method return value can never be null.
+ *    @ColumnInfo(name = "word")
+ *    Specify the name of the column in the table if you want it to be different from the name of the member variable.
+ *    Every field that's stored in the database needs to be either public or have a "getter" method.
+ *      This sample provides a getWord() method.
+ */
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
